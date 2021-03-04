e9f6ae71e3102befce7dc95dec5333781b4e1c05
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2021-03-04 23:01:36 +0900

    refactoring lambda

--- lambda/books_trigger.rb
+++ lambda/books_trigger.rb
@@ -2,10 +2,12 @@ require 'json'
 require 'aws-sdk'
 
 class BooksTrigger
+  VERSION = '1.1'.freeze
+
   SUMMERY_KEY = '#SUMMERY#'.freeze
 
   def run(event, _context)
-    # p event['Records']
+    p event['Records']
     return if event['Records'].size == 1 && summary?(event['Records'][0]['dynamodb'])
 
     total_cost = event['Records'].inject(0) { |total, rec| total + cost(rec['dynamodb']) }
@@ -29,21 +31,7 @@ class BooksTrigger
   end
 
   def update_total_cost(total_cost)
-    if summery_exist?
-      dynamodb.update_item(update_item_param(total_cost))
-    else
-      dynamodb.put_item(put_item_param(total_cost))
-    end
-  end
-
-  def put_item_param(total_cost)
-    {
-      table_name: 'books',
-      item: {
-        title: SUMMERY_KEY,
-        cost: total_cost
-      }
-    }
+    dynamodb.update_item(update_item_param(total_cost))
   end
 
   def update_item_param(total_cost)
@@ -53,22 +41,11 @@ class BooksTrigger
         title: SUMMERY_KEY
       },
       expression_attribute_values: {
-        ':total_cost' => total_cost
+        ':total_cost' => total_cost,
+        ':initial_cost' => 0
       },
-      update_expression: 'SET cost = cost + :total_cost'
+      update_expression: 'SET cost = if_not_exists(cost, :initial_cost) + :total_cost'
     }
-  end
-
-  def summery_exist?
-    resp = dynamodb.get_item(
-      {
-        key: {
-          'title' => SUMMERY_KEY
-        },
-        table_name: 'books'
-      }
-    )
-    resp.item
   end
 
   def dynamodb
@@ -77,6 +54,9 @@ class BooksTrigger
 end
 
 def handler(event:, context:)
-  handler = BooksTrigger.new
-  handler.run(event, context)
+  p self
+  p self.class
+  puts "BooksTrigger::VERSION = #{BooksTrigger::VERSION}"
+  books_trigger = BooksTrigger.new
+  books_trigger.run(event, context)
 end
